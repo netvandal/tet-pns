4f342e92ad2d09a034614ccc022e1f700e735bfa
netvandal <netvandal@a1bfeacc-e642-0410-8a6b-e5688ae5d96e>
2008-01-04 18:28:08 +0000

    initial commit

---
+++ src/tetPns/PetriNet.java
@@ -0,0 +1,10 @@
+package tetPns;
+
+/**
+ * This is the main class that enclose all the Petri net elements
+ * @author Michele Tameni, Alessio Troiano
+ * @
+ */
+public class PetriNet {
+
+}
---
+++ src/tetPns/TetPns.java
@@ -0,0 +1,20 @@
+package tetPns;
+
+/**
+ * tetPns is a simple Petri net simulator
+ * @author Michele Tameni, Alessio Troiano
+ * 
+ */
+public class TetPns {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola");
+		Flow test = new Flow();
+	}
+
+	
+}
