17a6c364aa3400e9ec55756d0c9f9e242c27093f
alessio.troy@gmail.com <alessio.troy@gmail.com@a1bfeacc-e642-0410-8a6b-e5688ae5d96e>
2008-04-24 09:28:00 +0000

    con rmi che funzione(...forse!)

--- trunk/src/tetServer/TetServer.java
+++ trunk/src/tetServer/TetServer.java
@@ -3,33 +3,46 @@
  */
 package tetServer;
 
-import java.rmi.Naming;
+import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 
 /**
  * @author michele
  *
  */
-public class TetServer {
+public class TetServer extends UnicastRemoteObject{
+
+	protected TetServer() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @param args
+	 * @throws AlreadyBoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AlreadyBoundException  {
 		try {
 			
 			Dispenser disp = new Dispenser();
 			Simulator sim = new Simulator();
 			
-			Naming.rebind("SIMULATOR", sim);
-			Naming.rebind("DISPENSER", disp);
+			//Naming.rebind("SIMULATOR", sim);
+			//Naming.rebind("DISPENSER", disp);
 		
+			Registry r = LocateRegistry.getRegistry();
+			r.bind("SIMULATOR", sim);
+			r.bind("DISPENSER", disp);
 		}
-		/* If the URL-formatted name is not legal... */
-		catch (java.net.MalformedURLException e) {
-			System.out.println("Malformed URL for TetServer name " + e.toString());
-		}
-		
 		/* If any communication failures occur... */
 		catch (RemoteException e) {
 			System.out.println("Communication error " + e.toString());
