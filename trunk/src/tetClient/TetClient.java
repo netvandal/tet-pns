6724f50bdab3f16114cf9664ee2d085d021a3fb1
alessio.troy@gmail.com <alessio.troy@gmail.com@a1bfeacc-e642-0410-8a6b-e5688ae5d96e>
2008-04-24 09:28:47 +0000

    tetClient

---
+++ trunk/src/tetClient/TetClient.java
@@ -0,0 +1,28 @@
+package tetClient;
+
+import java.rmi.RMISecurityManager;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import tetServer.IDispenser;
+
+
+public class TetClient {
+	public static void main(String[] args) {
+		
+		System.setSecurityManager(new RMISecurityManager());
+		
+		try {
+            Registry reg = LocateRegistry.getRegistry();
+            
+            IDispenser d = (IDispenser) reg.lookup("DISPENSER");
+            System.out.println(d.getNet("Ciao Ciao"));
+            
+        } catch (Exception e) {
+            // Something wrong here
+            e.printStackTrace();
+        }
+		
+	}
+	
+}
