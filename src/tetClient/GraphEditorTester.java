dedd07d6d52f596e46a401a6dad819a5b3e30043
netvandal <netvandal@a1bfeacc-e642-0410-8a6b-e5688ae5d96e>
2008-09-01 10:09:37 +0000

    Fix vari e clean up codice

--- src/tetClient/GraphEditorTester.java
+++ src/tetClient/GraphEditorTester.java
@@ -4,21 +4,30 @@ import javax.swing.JFrame;
 
 import tetPns.PetriNet;
 
+/**
+ * Classe utile alla gestione del grafico della rete di petri. 
+ * Si preoccupa di creare la finestra, impostarne le dimensione e renderla visibile
+ * Inoltre gestisce anche l'aggiornamento del grafico ad ogni passo della simulazione
+ * @author michele
+ *
+ */
 
 public class GraphEditorTester extends JFrame {
 	NetGraph graphEditor;
 	PetriNet pnz;
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 
 	public GraphEditorTester(PetriNet pn) {
+		
+		int altezzaFinestra = 500;
+		int largFinestra = 900;
+		
 		this.pnz = pn;
 		setTitle("T&TPnS Sim is fun... no, really!");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.graphEditor = new NetGraph(900, 500, pnz);
+		this.graphEditor = new NetGraph(largFinestra, altezzaFinestra, pnz);
 		getContentPane().add(graphEditor);
 		pack();
 		setVisible(true);
