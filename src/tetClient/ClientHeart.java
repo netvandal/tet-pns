3b239a0468d99c242a5b63859ce0048e64b884fa
netvandal <netvandal@a1bfeacc-e642-0410-8a6b-e5688ae5d96e>
2008-10-03 08:12:05 +0000

    File dimenticti 2

---
+++ src/tetClient/ClientHeart.java
@@ -0,0 +1,30 @@
+package tetClient;
+
+import tetServer.ISimulator;
+
+public class ClientHeart extends Thread  {
+
+	final static int SLEEPTIME =  10000; // 10 secondi
+	
+	ISimulator sim = null;
+	int id;
+
+	
+	public ClientHeart(ISimulator s, int idClient) {
+		sim = s;
+		id = idClient;
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				sim.imAlive(id);
+				Thread.sleep(SLEEPTIME);
+			}
+		} catch ( Exception e) {
+			 System.out.println("Errore di comunicazione con il server.\n Chiusura del programma.");
+			 System.exit(1);
+		}
+	}
+	
+}
